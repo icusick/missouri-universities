99c8f93fc078c8cc5391e7ddd1b331eefbd35325
hasmikcoder <hasvart@gmail.com>
2017-09-20 20:15:45 -0500

    Used Bootstrap to display the names of universities

--- src/App.js
+++ src/App.js
@@ -2,41 +2,47 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+
+
   data = [
-              {rank: 1, name: "WashU", location: "St.Louis"},
-              {rank: 2, name: "Universty of Missouri", location: "Columbia"},
-              {rank: 3, name: "University of Missouri-Kansas City", location: "Kansas City"},
-              {rank: 4, name: "Saint Louis University", location: "St.Louis"},
-              {rank: 5, name: "University of Missouri-St.Louis", location: "St.Louis"},
+              {rank: 1, name: " Washington University in St.Louis", location: "St.Louis"},
+              {rank: 2, name: " Universty of Missouri", location: "Columbia"},
+              {rank: 3, name: " University of Missouri-Kansas City", location: "Kansas City"},
+              {rank: 4, name: " Saint Louis University", location: "St.Louis"},
+              {rank: 5, name: " University of Missouri-St.Louis", location: "St.Louis"},
               {rank: 6, name: " Missouri State University", location: "Springfield"},
               {rank: 7, name: " Missouri University of Science and Technology", location: "Rolla"},
-              {rank: 8, name: "Webster University", location: "St.Louis"},
-              {rank: 9, name: "Truman State University", location: "Kirksville"},
-              {rank: 10, name: "Southeast Missouri State University", location: "Cape Girardeau"}
+              {rank: 8, name: " Webster University", location: "St.Louis"},
+              {rank: 9, name: " Truman State University", location: "Kirksville"},
+              {rank: 10, name: " Southeast Missouri State University", location: "Cape Girardeau"}
   ];
 
-  render() {
+   render() {
 
+      return (
 
-    return (
-      <div>
-        <ul>
-            {this.data.map(function(university, index){
-              return (<li key={ index }>{university.name}</li>);
-            })}
-        </ul>
-
-
-        <div className="App">
-          <div className="App-header">
-
-            <img src="App-logo" alt="logo" />
-            <h2>Welcome to top 10 universities in MO......</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload
-          </p>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4"> Rank</div>
+          <div className="col-md-4"> University </div>
+          <div className="col-md-4"> Location </div>
         </div>
+
+
+
+
+            {this.data.map(function(University, index){
+            return (
+              <div className="row" key={ index }>
+                <div className="col-md-4">{University.rank}</div>
+                <div className="col-md-4">{University.name}</div>
+                <div className="col-md-4">{University.location}</div>
+              </div>);
+            })}
+
+
+
        </div>
     );
   }
